24119028f6d22b821ca40efbfead436d818d8278
Bedo-Mostafa <bedomostafa37@gmail.com>
2024-04-25 01:01:23 +0200

    make some state for Ai

--- Assets/Scripts/Character/NPC/StatesForNPC/AiDanceState.cs
+++ Assets/Scripts/Character/NPC/StatesForNPC/AiDanceState.cs
@@ -1,24 +1,43 @@
 using System.Collections;
-using System.Collections.Generic;
+// using System.Collections.Generic;
+using System;
+// using TMPro;
 using UnityEngine;
+using UnityEngine.Animations.Rigging;
 
 public class AiDanceState : AiState
 {
+    private float danceStartTime;
     public AiStateId GetId()
     {
-        throw new System.NotImplementedException();
+        return AiStateId.Dance;
     }
     public void Enter(AiAgent agent)
     {
-        throw new System.NotImplementedException();
+        agent.navMeshAgent.speed = 0;
+        agent.animator.SetLayerWeight(1,1);
+        agent.RigLayer.GetComponent<Rig>().weight = 0;
+        agent.Weapon.SetActive(false);
+
+        danceStartTime = Time.time;
+        agent.StartCoroutine(TimeDance(agent));
     }
     public void Update(AiAgent agent)
     {
-        throw new System.NotImplementedException();
     }
 
     public void Exit(AiAgent agent)
     {
-        throw new System.NotImplementedException();
+    }
+    IEnumerator TimeDance(AiAgent agent)
+    {
+        yield return new WaitForSeconds(5);
+        // Debug.Log("yyy");
+        agent.navMeshAgent.speed = 7;
+        agent.animator.SetLayerWeight(1,0);
+        agent.RigLayer.GetComponent<Rig>().weight = 1;
+        agent.Weapon.SetActive(true);
+        agent.initialState = AiStateId.ChasePlayer;
     }
 }
+
--- Assets/Scripts/Character/NPC/ِِِِAI/AiAgent.cs
+++ Assets/Scripts/Character/NPC/ِِِِAI/AiAgent.cs
@@ -8,30 +8,60 @@ public class AiAgent : MonoBehaviour
     public AiStateMachine stateMachine;
     public AiStateId initialState;
     [HideInInspector]
-    public NavMeshAgent navMeshAgent ;
+    public NavMeshAgent navMeshAgent;
     public AiAgentConfig config;
 
-    [SerializeField] public Transform PlayerTransform;
-    [SerializeField] public Transform KeyTransform;
+
+    [HideInInspector] public Transform PlayerTransform;
+    [HideInInspector] public Transform KeyTransform;
+    [HideInInspector] public Transform FinalGoalTransform;
+    // public AiLocomotion aiLocomotion;
+    public GameObject Weapon;
+    // public GameObject NPC;
+    public GameObject RigLayer;
+    public Animator animator;
+    public bool hasDoorLockedKey = false;
+
     // Start is called before the first frame update
     void Start()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
         stateMachine = new AiStateMachine(this);
         PlayerTransform = GameObject.FindGameObjectWithTag("Player").transform;
-        KeyTransform = GameObject.FindGameObjectWithTag("Key").transform;
+        FinalGoalTransform = GameObject.FindGameObjectWithTag("DoorGoal").transform;
+        KeyTransform = GameObject.Find("KeyDoor").transform;
+
         // Create instance from this state
         stateMachine.RegisterState(new AiGetKeyState());
         stateMachine.RegisterState(new AiChasePlayerState());
+        stateMachine.RegisterState(new AiGoToFinalGoalState());
+        stateMachine.RegisterState(new AiDanceState());
         // stateMachine.RegisterState(new AiDeathState());
-
+        initialState = AiStateId.goToKey;
         stateMachine.changeState(initialState);
     }
 
     // Update is called once per frame
     void Update()
     {
-        // stateMachine.changeState(initialState);
+        if (hasDoorLockedKey)
+        {
+            // Debug.Log("Bedo");
+            initialState = AiStateId.goToFinalGoal;
+            stateMachine.changeState(initialState);
+        }
+        stateMachine.changeState(initialState);
         stateMachine.Update();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Debug.Log("1");
+        if (other.gameObject.CompareTag("DoorInteractiveObj"))
+        {
+            // Debug.Log("Bedo");
+            hasDoorLockedKey = true;
+            Destroy(other.gameObject);
+        }
     }
 }
