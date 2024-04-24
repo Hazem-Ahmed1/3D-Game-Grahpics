74250bb69eeefd91c60ec5a68acac02390230989
Bedo-Mostafa <bedomostafa37@gmail.com>
2024-04-24 05:16:08 +0200

    door

--- Assets/Scripts/Door Pack/DoorController.cs
+++ Assets/Scripts/Door Pack/DoorController.cs
@@ -6,24 +6,22 @@ public class DoorController : MonoBehaviour
 {
     private Animator animator_Door;
     private bool DoorOpen = false;
-    [SerializeField] private AudioSource audioSource = null;
-    [SerializeField] private AudioClip audioClip = null;
+    // [SerializeField] private AudioSource audioSource = null;
+    // [SerializeField] private AudioClip audioClip = null;
     [SerializeField] private int waitTimerAnimation = 1;
     [SerializeField] private bool pauseInteraction = false;
     // Start is called before the first frame update
     void Start()
     {
         animator_Door = gameObject.GetComponent<Animator>();
-
-        
     }
 
     public void PlayAnimation()
     {
         if (!DoorOpen && animator_Door != null && !pauseInteraction) 
         {
-            audioSource.clip = audioClip;
-            audioSource.Play();
+            // audioSource.clip = audioClip;
+            // audioSource.Play();
             animator_Door.Play("OpenDoor", 0, 0.0f);
             DoorOpen = true;
             StartCoroutine(PauseDoorInteraction());
@@ -31,8 +29,8 @@ public class DoorController : MonoBehaviour
         }
         else if (DoorOpen && animator_Door != null && !pauseInteraction)
         {
-            audioSource.clip = audioClip;
-            audioSource.Play();
+            // audioSource.clip = audioClip;
+            // audioSource.Play();
             animator_Door.Play("CloseDoor", 0, 0.0f);
             DoorOpen = false;
             StartCoroutine(PauseDoorInteraction());
--- Assets/Scripts/Door Pack/KeyItemController.cs
+++ Assets/Scripts/Door Pack/KeyItemController.cs
@@ -7,13 +7,13 @@ public class KeyItemController : MonoBehaviour
     [SerializeField] private bool Door = false;
     [SerializeField] private bool Key = false;
     [SerializeField] private KeyInventory PlayerInventory = null;
-    [SerializeField] private KeyInventory EnemyInventory = null;
+    // [SerializeField] private KeyInventory EnemyInventory = null;
     [SerializeField]GameObject Player;
-    [SerializeField]GameObject Enemy;
+    // [SerializeField]GameObject Enemy;
     // private string interactTableTagEnemy = "EnemyInteractiveObj";
     // private string interactTableTagPlayer = "Player";
     private float distancePlayer;
-    private float distanceEnemy;
+    // private float distanceEnemy;
     private KeyDoorController doorObject;
 
     private void Start()
@@ -33,16 +33,17 @@ public class KeyItemController : MonoBehaviour
         else if (Key)
         {
             distancePlayer = Vector3.Distance(Player.gameObject.transform.position, this.gameObject.transform.position);
-            distanceEnemy = Vector3.Distance(Enemy.gameObject.transform.position, this.gameObject.transform.position);
-            Debug.Log(distanceEnemy);
+            // distanceEnemy = Vector3.Distance(Enemy.gameObject.transform.position, this.gameObject.transform.position);
+            // Debug.Log(distanceEnemy);
             Debug.Log(distancePlayer);
             if (distancePlayer <= 5)
             {
                 PlayerInventory.hasDoorLockedKey = true;
-            }else if (distanceEnemy <= 5)
-            {
-                EnemyInventory.hasDoorLockedKey=true;
             }
+            // else if (distanceEnemy <= 5)
+            // {
+            //     EnemyInventory.hasDoorLockedKey=true;
+            // }
             gameObject.SetActive(false);
         }
     }
